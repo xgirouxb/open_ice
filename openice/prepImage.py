c48a457daddf3fcb291299812851050836648836
xgirouxb <xgirouxb@gmail.com>
2022-11-16 12:11:19 -0800

    Add img prep modules
    Scripts with functions to add properties, bands, or apply masks.

---
+++ openice/prepImage.py
@@ -0,0 +1,115 @@
+# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+# Collection of functions required to prepare imagery for analysis
+# 
+# Functions designed for Landsat 7 + 8 TOA and Sentinel 2 TOA
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Initialize EE API
+import ee
+ee.Initialize()
+
+# ---------------------------------------------------------------------------- #
+# Global water masking function
+
+# Import global water JRC layer
+globalWater = ee.Image('JRC/GSW1_3/GlobalSurfaceWater')\
+                          .select('occurrence').gte(80)
+
+# Import Large Scale International Boundary Polygons for NA, convert to raster
+filterNorthAm = ee.Filter.inList('country_co', ['CA', 'US'])
+northAm = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017").filter(filterNorthAm)
+northAmCoastline = ee.Image(0).byte().paint(northAm, 1)
+
+# Water mask function
+def waterMask(img):
+    """
+    This function updates an image's mask so it only contains freshwater pixels.
+    Uses permanent water (occurence 80-100 %) in the JRC global surface water dataset
+    and removes water pixels in marine coastal areas (using political boundary coastlines) 
+    see manual: https://storage.googleapis.com/global-surface-water/downloads_ancillary/DataUsersGuidev2.pdf
+    """
+    return img.updateMask(globalWater.updateMask(northAmCoastline))
+
+# ---------------------------------------------------------------------------- #
+# Tile mask function
+def tileMask(imgCol, tile):
+    """
+    This function masks out the areas outside of the provided tile (geometry),
+    for each img in an imgCol.
+    """
+    # Create mask of tile
+    mask = ee.Image(0).byte().paint(tile, 1)
+    # Function to map over collection
+    def mapTileMask(img):
+        # update image mask
+        return img.updateMask(mask)
+    # return imgCol
+    return imgCol.map(mapTileMask)
+
+# ---------------------------------------------------------------------------- #
+# Add dates (day of year and year) as bands
+def addDate(img):
+    """
+    This function adds both the year and the day of year (doy) as bands to an image.
+    """
+    # Get the day of year and year, convert to unsigned 16 bit integer
+    doy = img.date().getRelative('day', 'year').add(1).toUint16()
+    year = img.date().get('year').toUint16()
+    # Create single banded image for each
+    doyBand = ee.Image(doy).rename(['doy'])
+    yearBand = ee.Image(year).rename(['year'])
+    # Add bands to image
+    return img.addBands([doyBand, yearBand])
+
+# ---------------------------------------------------------------------------- #
+# Add day of year as band
+def addDoy(img):
+    """
+    This function adds the day of year (doy) as a bands to an image.
+    """
+    # Get the day of year, convert to unsigned 16 bit integer
+    doy = img.date().getRelative('day', 'year').add(1).toUint16()
+    # Create image with doy as band
+    doyBand = ee.Image(doy).rename(['doy'])
+    # Add band to image
+    return img.addBands(doyBand)
+
+# ---------------------------------------------------------------------------- #
+# Add dates (day of year and year) as bands
+def addYear(img):
+    """
+    This function adds the year as a band to an image.
+    """
+    # Get the year, convert to unsigned 16 bit integer
+    year = img.date().get('year').toUint16()
+    # Create single banded image
+    yearBand = ee.Image(year).rename(['year'])
+    # Add band to image
+    return img.addBands(yearBand)
+    
+# ---------------------------------------------------------------------------- #
+# Add fractional year since epoch as band
+def addFracYear(img):
+    """
+    This function converts seconds since epoch to fractional year
+    and adds it as a band to an image.
+    """
+    # Fractional years since the epoch
+    fracYear = img.date().difference(ee.Date('1970-01-01'), 'year').add(1970)
+    # Create single band image, rename
+    fracYearBand = ee.Image(fracYear).float().rename(['fracYear'])
+    # Add band
+    return img.addBands(fracYearBand)
+
+# ---------------------------------------------------------------------------- #
+# Add date (YYYY-MM-dd) as property
+def addDateProp(img):
+    """
+    This function adds the date (format YYYY-MM-dd) as a property to an image.
+    """
+    # Extract date from 'system:time_start'
+    date = img.date().format('YYYY-MM-dd')
+    # Add property
+    return img.set('date', date)
---
+++ openice/prepOpticalBands.py
@@ -0,0 +1,112 @@
+# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+# Collection of functions required to prepare optical bands for analyses,
+# and add bands or indices specific to optical imagery
+# 
+# Functions designed for Landsat 8 and Sentinel 2 imagery
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Initialize EE API
+import ee
+ee.Initialize()
+
+# ---------------------------------------------------------------------------- #
+# Setup required parameters
+
+# List of properties to copy
+propList = ['system:index', 'system:time_start', 'system:footprint']
+
+# Bands for this script
+l7Bands  =   [  'B1',    'B2',  'B3',  'B4',    'B5',    'B7'] # Landsat 7
+l8Bands  =   [  'B2',    'B3',  'B4',  'B5',    'B6',    'B7'] # Landsat 8
+s2Bands  =   [  'B2',    'B3',  'B4',  'B8',   'B11',   'B12'] # Sentinel 2
+stdBands =   ['blue', 'green', 'red', 'nir', 'swir1', 'swir2'] # Common band names
+
+# ---------------------------------------------------------------------------- #
+# Rename bands, mask out bad values, and gap fill Landsat 7 TOA
+def prepL7TOA(img):
+    '''
+    This function prepares Landsat 7 Top of Atmosphere images
+    for further analysis by masking edge pixels with incomplete
+    data, gap filling 90m of L7 SLC off data, and renaming bands.
+    '''
+    # Rename bands
+    img = img.select(l7Bands, stdBands)
+  
+    # Mask out bad reflectance values (< 0 or >1)
+    toa = img.updateMask(img.gte(0).And(img.lte(1)))
+  
+    # Mask out incomplete pixels
+    toa = toa.updateMask(toa.mask().reduce(ee.Reducer.min()))
+  
+    # Fill L7 SLC off gaps using focal mean
+    toa = ee.Image(toa.focal_mean(2, 'circle', 'pixels', 8)).blend(toa)
+
+    # Return clipped and masked, set product property
+    return img.addBands(toa, stdBands, True)\
+              .clip(toa.geometry().buffer(-2000))\
+              .copyProperties(img, propList)\
+              .set('product', 'l7toa')
+
+# ---------------------------------------------------------------------------- #
+# Scale, rename, and mask out bad values in Landsat 8 TOA
+def prepL8TOA(img):
+    """
+    This function prepares Landsat 8 Top of Atmosphere images
+    for further analysis by scaling bands B2 to B7 to
+    a 0 - 1 range and masking out bad pixels.
+    """
+    # Select/rename bands
+    img = img.select(l8Bands, stdBands);
+  
+    # Mask out bad reflectance values (< 0 or >1)
+    toa = img.updateMask(img.gte(0).And(img.lte(1)))
+  
+    # Mask out incomplete pixels
+    toa = toa.updateMask(toa.mask().reduce(ee.Reducer.min()))
+  
+    # Return, copy properties, set product property
+    return img.addBands(toa, stdBands, True)\
+              .copyProperties(img, propList)\
+              .set('product', 'l8toa')
+        
+# ---------------------------------------------------------------------------- #
+# Scale, rename, and mask out bad values in Sentinel 2 TOA
+def prepS2TOA(img):
+    """
+    This function prepares Sentinel 2 Top of Atmosphere Reflectance images
+    for further analysis by scaling bands B2, B3, B4, B8, B11, and B12 to
+    a 0 - 1 range and masking out bad pixels.
+    """
+    # Select/rename bands
+    img = img.select(s2Bands, stdBands)
+  
+    # Mask out bad reflectance values (< 0 or >10000)
+    toa = img.updateMask(img.gte(0).And(img.lte(10000)))
+  
+    # Mask out incomplete pixels
+    toa = toa.updateMask(toa.mask().reduce(ee.Reducer.min()))
+  
+    # Scale to range 0 - 1
+    toa = toa.unitScale(0, 10000)
+                
+    # Return, copy properties, set product property
+    return img.addBands(toa, stdBands, True)\
+              .copyProperties(img, propList)\
+              .set('product', 's2toa')
+
+# ---------------------------------------------------------------------------- #
+# Add Normalized Difference Snow Index
+def addNDSI(img):
+    """
+    This function adds the Normalized Difference Snow Index 
+    (aka modified Normalized Difference Water Index, Xu 2006)
+    as a band to optical images with standard band names (green and swir1).
+    The values are scaled to [0, 1] range for use in classifiers.   
+    """
+    # Compute NDSI, scale to [0-1] range for classifiers
+    ndsi = img.normalizedDifference(['green','swir1'])\
+              .unitScale(-1, 1)\
+              .rename('ndsi')
+    return img.addBands(ndsi)
